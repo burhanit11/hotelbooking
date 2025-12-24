d93cedb33bc6d2ad381dc4c0d3559f6f53c88e6b
Burhan Rabbani <burhanit11@gmail.com>
2025-12-24 09:55:18 +0500

    up

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,1 +1,6 @@
-export const server = "http://localhost:5000/api/v1";
+// export const server = "http://localhost:5000/api/v1";
+
+export const server =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api/v1"
+    : "https://solarbazarbackend.vercel.app/api/v1";
